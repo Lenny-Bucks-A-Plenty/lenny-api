a66b25abaf8aa74069a87441275c921cb08d1a9f
Charles Buffington <dev.cfsb@proton.me>
2024-06-07 16:32:11 -0400

    add stuff to make things work stuff is still broke bc classes

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from app import api, db_engine
 from app.database import SP500DataTable
 from app.analysis_algo.v2 import do_shit
 from sqlmodel import Session, select
+import yfinance as yf
 
 @api.get('/')
 def index():
@@ -21,4 +22,15 @@ def sp500():
 @api.get('/take')
 def getLennyTake(ticker: str):
     res = do_shit(ticker=ticker)
+    return { 'action': res[0], 'explanation': res[1] }
+
+@api.get('/graph')
+def getStockGraph(ticker: str):
+    yticker = yf.Ticker(ticker=ticker)
+    df = yticker.history(interval='1d', period='2y')[["Close"]]
+
+    res = []
+    for idx, row in df.iterrows():
+        res.append({'timestamp': str(idx), 'price': row['Close']})
+
     return res
